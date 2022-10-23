8b37beb355ca5a4bc48d36edb3b78e0ae5322029
Caio Villa Silva Tsubake <caiotsubake@gmail.com>
2022-10-23 18:25:32 -0300

    Lendo primeiras linhas do arquivo
    A aplicação ira ler as primeiras 5 linhas e escrever no console para podermos testar que os dados do arquvo
     estão sendo recebidos corretamente

---
+++ AuditoriaLogsBackend/EntradaDeLogs/FileWrapper.cs
@@ -0,0 +1,20 @@
+﻿using EntradaDeLogs.Interfaces;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace EntradaDeLogs
+{
+    public class FileWrapper : IFileWrapper
+    {
+        public bool Exists(string path)
+        {
+            return File.Exists(path);
+        }
+
+        public List<string> ReadLines(string path)
+        {
+            return File.ReadLines(path).ToList();
+        }
+    }
+}
--- AuditoriaLogsBackend/EntradaDeLogs/Program.cs
+++ AuditoriaLogsBackend/EntradaDeLogs/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace EntradaDeLogs
 {
@@ -10,9 +6,17 @@ namespace EntradaDeLogs
     {
         static void Main(string[] args)
         {
+            FileWrapper fileWrapper = new FileWrapper();
+            LogReader logReader = new LogReader(fileWrapper);
             string filePath = @"C:\Users\caiot\Documents\Profissional\Code\Accenture\auth.log";
             Console.WriteLine($"Lendo arquivo: {filePath}");
 
+            var linhas = fileWrapper.ReadLines(filePath);
+            // Lendo 5 linhas do arquivo como teste
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"Linha: {linhas[i]}");
+            }
 
             Console.ReadLine();
 
