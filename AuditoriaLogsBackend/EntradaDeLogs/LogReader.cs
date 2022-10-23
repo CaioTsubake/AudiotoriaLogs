859e6ebab314e8794eea92c3d69bf0031aef44ed
Caio Villa Silva Tsubake <caiotsubake@gmail.com>
2022-10-23 19:35:23 -0300

    Removendo referencias não usadas

--- AuditoriaLogsBackend/EntradaDeLogs/LogReader.cs
+++ AuditoriaLogsBackend/EntradaDeLogs/LogReader.cs
@@ -1,10 +1,6 @@
 ﻿using EntradaDeLogs.Interfaces;
-using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace EntradaDeLogs
 {
