09da362972a4dab93064b887154491a88ae4dede
Caio Villa Silva Tsubake <caiotsubake@gmail.com>
2022-10-23 18:36:13 -0300

    Criado projeto de conexão com o banco
    Ele tem a responsábilidade de fazer a conexão com o banco, bem como realizar todas as operações dentro do banco. Foi criada também a classe que contém o objeto para ser salvo no banco

---
+++ AuditoriaLogsBackend/ConexaoDb/Model/AuditoriaLog.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConexaoDb
+{
+    public class AuditoriaLog
+    {
+        public int Id { get; set; }
+        public DateTime DataHora { get; set; }
+        public string Ip { get; set; }
+        public string Tipo { get; set; }
+        public string Mensagem { get; set; }
+
+    }
+}
