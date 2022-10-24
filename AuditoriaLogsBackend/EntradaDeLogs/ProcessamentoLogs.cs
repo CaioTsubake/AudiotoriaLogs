aa684445a9059b24582a9c2999ee25a883621d8c
Caio Villa Silva Tsubake <caiotsubake@gmail.com>
2022-10-24 20:01:48 -0300

    Testando busca por datas usando console

--- AuditoriaLogsBackend/EntradaDeLogs/ProcessamentoLogs.cs
+++ AuditoriaLogsBackend/EntradaDeLogs/ProcessamentoLogs.cs
@@ -24,9 +24,12 @@ namespace EntradaDeLogs
             }
 
             var logsAuditoria = preparacao.ConverterLogs(linhas);
-            conexao.InserirLogs(logsAuditoria);
 
-            var retornoBanco = conexao.GetLogs();
+            var dataInicial = DateTime.Parse("2022-11-30 06:00:01");
+            var dataFinal = DateTime.Parse("2022-11-30 06:47:01");
+
+
+            var retornoBanco = conexao.BuscarPorDatas(dataInicial, dataFinal);
             foreach (var dadosBanco in retornoBanco)
             {
                 Console.WriteLine($"Dados do Banco: Id:{dadosBanco.Id} Data:{dadosBanco.DataHora} Ip:{dadosBanco.Ip} " +
