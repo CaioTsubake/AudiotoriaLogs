9e2d79d6f5a9928248a8af0875fee0cf20a5423c
Caio Villa Silva Tsubake <caiotsubake@gmail.com>
2022-10-24 20:29:28 -0300

    Adicionado endpoint para buscar por mensagem
    A chamada para o método do banco foi movida para
    uma classe mais interna, para ser mais fácil controlar,
    testar e dar manutenção estando fora do controller

--- AuditoriaLogsBackend/ApiWeb/Controllers/LogsController.cs
+++ AuditoriaLogsBackend/ApiWeb/Controllers/LogsController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using ConexaoDb;
 
 namespace ApiWeb.Controllers
 {
@@ -12,6 +13,15 @@ namespace ApiWeb.Controllers
         public string GetHeartbeat() 
         {
             return "Ok";
+        }
+
+        public List<AuditoriaLog> GetLogs(string mensagem) 
+        {
+            LogsActions logsActions = new LogsActions();
+
+            List<AuditoriaLog> resultado = logsActions.BuscarLogsComMensagem(mensagem);
+
+            return resultado;
         }
     }
 }
---
+++ AuditoriaLogsBackend/ApiWeb/LogsActions.cs
@@ -0,0 +1,19 @@
+﻿using ConexaoDb;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ApiWeb
+{
+    public class LogsActions
+    {
+        public List<AuditoriaLog> BuscarLogsComMensagem(string mensagem) 
+        {
+            OperacoesLogs conexao = new OperacoesLogs();
+            List<AuditoriaLog> resultadoBusca = conexao.BuscarPorMensagem(mensagem);
+
+            return resultadoBusca;
+        }
+    }
+}
