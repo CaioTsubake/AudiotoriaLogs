c3adddb5249f4aaccc2ecdb4ec05a900e149f376
Caio Villa Silva Tsubake <caiotsubake@gmail.com>
2022-10-23 21:32:52 -0300

    Obtendo Ip dos dados do Log

--- AuditoriaLogsBackend/EntradaDeLogs/PreparacaoSalvar.cs
+++ AuditoriaLogsBackend/EntradaDeLogs/PreparacaoSalvar.cs
@@ -1,4 +1,5 @@
 ﻿using ConexaoDb;
+using System;
 
 namespace EntradaDeLogs
 {
@@ -7,6 +8,15 @@ namespace EntradaDeLogs
         public AuditoriaLog ParseLog(string linha) 
         {
             AuditoriaLog log = new AuditoriaLog();
+
+            // os primeiros 15 digitos são sempre a data
+            // o primeiro espaço depois da data mostra onde o ip termina
+            var dadosSemData = linha.Remove(0, 16);
+            var fimIp = dadosSemData.IndexOf(' ');
+            var stringIp = dadosSemData.Substring(0, fimIp);
+            // Removendo o "ip-" da linha
+            stringIp = stringIp.Replace("ip-", "");
+            log.Ip = stringIp;
 
             var inicioMensagem = linha.IndexOf(": ");
             // Avançando 2 caracteres para não incluir ": "
--- AuditoriaLogsBackend/EntradaDeLogsTests/PreparacaoSalvarTests.cs
+++ AuditoriaLogsBackend/EntradaDeLogsTests/PreparacaoSalvarTests.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 
 namespace EntradaDeLogs.Tests
 {
@@ -18,5 +19,20 @@ namespace EntradaDeLogs.Tests
             // Assert
             Assert.AreEqual("pam_unix(cron:session): session opened for user root by (uid=0)", resultado.Mensagem);
         }
+
+        [TestMethod()]
+        public void ParseLog_RetornaLogComIp_QuandoRecebeStringLog()
+        {
+            // Arrange
+            string log = @"Nov 30 16:17:01 ip-172-31-27-153 CRON[22885]: pam_unix(cron:session): session opened for user root by (uid=0)";
+            PreparacaoSalvar preparacao = new PreparacaoSalvar();
+
+            // Act
+            var resultado = preparacao.ParseLog(log);
+
+            // Assert
+            Assert.AreEqual("172-31-27-153", resultado.Ip);
+        }
+
     }
 }
