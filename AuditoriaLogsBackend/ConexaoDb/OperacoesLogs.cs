12bfe360b183e289c9b2dc939750a4bb7748b7bc
Caio Villa Silva Tsubake <caiotsubake@gmail.com>
2022-10-24 20:00:16 -0300

    Criado método de busca por datas
    Ele será usado para buscar no banco logs cuja data
    se encontra em um determinado periodo de datas

--- AuditoriaLogsBackend/ConexaoDb/OperacoesLogs.cs
+++ AuditoriaLogsBackend/ConexaoDb/OperacoesLogs.cs
@@ -10,11 +10,11 @@ namespace ConexaoDb
 {
     public class OperacoesLogs
     {
+        private readonly string _connectionString = @"Server=.\SQLEXPRESS;Database=AuditoriaLogs;Trusted_Connection=True;";
+
         public List<AuditoriaLog> GetLogs() 
         {
-            var connectionString = @"Server=.\SQLEXPRESS;Database=AuditoriaLogs;Trusted_Connection=True;";
-
-            using (var conexao = new SqlConnection(connectionString))
+            using (var conexao = new SqlConnection(_connectionString))
             {
                 var resultado = conexao.Query<AuditoriaLog>("SELECT * FROM Logs").ToList();
 
@@ -24,10 +24,9 @@ namespace ConexaoDb
 
         public void InserirLogs(List<AuditoriaLog> logs) 
         {
-            var connectionString = @"Server=.\SQLEXPRESS;Database=AuditoriaLogs;Trusted_Connection=True;";
             var query = "INSERT INTO Logs VALUES (@DataHora, @Ip, @Tipo, @Mensagem)";
 
-            using (var conexao = new SqlConnection(connectionString))
+            using (var conexao = new SqlConnection(_connectionString))
             {
                 conexao.Execute(query, logs);
             }
@@ -35,10 +34,21 @@ namespace ConexaoDb
 
         public List<AuditoriaLog> BuscarPorMensagem(string mensagem) 
         {
-            var connectionString = @"Server=.\SQLEXPRESS;Database=AuditoriaLogs;Trusted_Connection=True;";
             var query = $"SELECT * FROM Logs WHERE Mensagem LIKE '%{mensagem}%'";
 
-            using (var conexao = new SqlConnection(connectionString))
+            using (var conexao = new SqlConnection(_connectionString))
+            {
+                var resultado = conexao.Query<AuditoriaLog>(query).ToList();
+
+                return resultado;
+            }
+        }
+
+        public List<AuditoriaLog> BuscarPorDatas(DateTime dataInicial, DateTime dataFinal)
+        {
+            var query = $"SELECT * FROM Logs WHERE DataHora BETWEEN '{dataInicial}' AND '{dataFinal}'";
+
+            using (var conexao = new SqlConnection(_connectionString))
             {
                 var resultado = conexao.Query<AuditoriaLog>(query).ToList();
 
