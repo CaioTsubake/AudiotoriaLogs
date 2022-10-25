970878742175c155c64e9e70db8bc1709ceaf10f
Caio Villa Silva Tsubake <caiotsubake@gmail.com>
2022-10-24 21:37:40 -0300

    Adicionando tabela na tela de logs
    Esta tabela retirada de um exemplo vai servir de base
    para a tabela que mostrará os logs vindos da aplicação

--- AuditoriaLogsFrontend/src/app/app.module.ts
+++ AuditoriaLogsFrontend/src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { TelaLogsComponent } from './tela-logs/tela-logs.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatTableModule} from '@angular/material/table';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +13,9 @@ import { TelaLogsComponent } from './tela-logs/tela-logs.component';
     TelaLogsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
